85c82c9ee45e72183b19fda594d6ac463136abd8
Vivek Gemini <vivekgemini@gmail.com>
2022-01-30 18:06:06 +0400

    first terraform code

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+  }
+}
+provider "google" {
+  project = var.project
+  region  = var.region
+  zone    = var.zone
+}
+
+resource "google_compute_instance" "webserver" {
+  name = "myfirstvm"
+  machine_type = var.machine_type
+  boot_disk {
+    initialize_params {
+      image = var.image
+    }
+  }
+  network_interface {
+    network = "management-vpc"
+    subnetwork = "test"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "project" {
+  type    = string
+  default = "50550351381"
+}
+
+variable "region" {
+  type    = string
+  default = "us-central1"
+}
+
+variable "zone" {
+  type    = string
+  default = "us-central1-c"
+}
+
+variable "machine_type" {
+  type    = string
+  default = "f1-micro"
+}
+
+variable "image" {
+  type    = string
+  default = "debian-cloud/debian-9"
+}
